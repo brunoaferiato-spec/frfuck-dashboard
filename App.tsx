7f9b3a9bfc8becd19b50d5e592832fe78551a5ea
brunoaferiato-spec <bruno.a.feriato@gmail.com>
2026-03-22 18:40:40 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,41 +1,28 @@
-import { Toaster } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
-import ErrorBoundary from "./components/ErrorBoundary";
-import { ThemeProvider } from "./contexts/ThemeContext";
-import Home from "./pages/Home";
-import FolhaPagamento from "./pages/FolhaPagamento";
-import GestaoFuncionarios from "./pages/GestaoFuncionarios";
-import GestaoMetas from "./pages/GestaoMetas";
-import AnaliseFuncionario from "./pages/AnaliseFuncionario";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path={"/"} component={Home} />
-      <Route path={"/folha-pagamento"} component={FolhaPagamento} />
-      <Route path={"/funcionarios"} component={GestaoFuncionarios} />
-      <Route path={"/metas"} component={GestaoMetas} />
-      <Route path={"/analise-funcionario"} component={AnaliseFuncionario} />
-      <Route path={"/404"} component={NotFound} />
-      {/* Final fallback route */}
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
+import FolhaPagamento from "./FolhaPagamento";
+import GestaoFuncionarios from "./GestaoFuncionarios";
+import GestaoMetas from "./GestaoMetas";
+import AnaliseFuncionario from "./AnaliseFuncionario";
 
 function App() {
-  return (
-    <ErrorBoundary>
-      <ThemeProvider defaultTheme="dark">
-        <TooltipProvider>
-          <Toaster />
-          <Router />
-        </TooltipProvider>
-      </ThemeProvider>
-    </ErrorBoundary>
-  );
+  const path = typeof window !== "undefined" ? window.location.pathname : "/";
+
+  if (path === "/folha-pagamento") {
+    return <FolhaPagamento />;
+  }
+
+  if (path === "/funcionarios") {
+    return <GestaoFuncionarios />;
+  }
+
+  if (path === "/metas") {
+    return <GestaoMetas />;
+  }
+
+  if (path === "/analisar-funcionario") {
+    return <AnaliseFuncionario />;
+  }
+
+  return <FolhaPagamento />;
 }
 
 export default App;
