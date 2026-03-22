1dea506f04b23448f6559b3d83deb76980e3cb52
brunoaferiato-spec <bruno.a.feriato@gmail.com>
2026-03-22 18:46:31 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,27 +1,6 @@
 import FolhaPagamento from "./FolhaPagamento";
-import GestaoFuncionarios from "./GestaoFuncionarios";
-import GestaoMetas from "./GestaoMetas";
-import AnaliseFuncionario from "./AnaliseFuncionario";
 
 function App() {
-  const path = typeof window !== "undefined" ? window.location.pathname : "/";
-
-  if (path === "/folha-pagamento") {
-    return <FolhaPagamento />;
-  }
-
-  if (path === "/funcionarios") {
-    return <GestaoFuncionarios />;
-  }
-
-  if (path === "/metas") {
-    return <GestaoMetas />;
-  }
-
-  if (path === "/analisar-funcionario") {
-    return <AnaliseFuncionario />;
-  }
-
   return <FolhaPagamento />;
 }
 
