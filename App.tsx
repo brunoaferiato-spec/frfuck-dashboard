a9204965681b37f583b21d0ba1b26e3e16350b17
brunoaferiato-spec <bruno.a.feriato@gmail.com>
2026-03-23 15:33:11 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// IMPORTS DAS SUAS TELAS (ajuste se algum nome estiver diferente)
+import AnaliseFuncionario from "./AnaliseFuncionario";
 import FolhaPagamento from "./FolhaPagamento";
+import GestaoFuncionarios from "./GestaoFuncionarios";
+import GestaoMetas from "./GestaoMetas";
 
 function App() {
-  return <FolhaPagamento />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<GestaoFuncionarios />} />
+        <Route path="/funcionarios" element={<GestaoFuncionarios />} />
+        <Route path="/folha" element={<FolhaPagamento />} />
+        <Route path="/metas" element={<GestaoMetas />} />
+        <Route path="/analise" element={<AnaliseFuncionario />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
